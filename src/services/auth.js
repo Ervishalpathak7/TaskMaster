eb7609ae0b3e67883c24a759e043f2ae9e425f03
Vishal Pathak <vishalpathak.er@gmail.com>
2025-03-21 12:21:27 +0530

    change username to email in auth service

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,9 +4,9 @@ import { findUserByEmail } from "./database.js";
 
 
 export default  passport.use(
-  new Strategy(async (username, password, done) => {
+  new Strategy(async (email, password, done) => {
     try {
-      let finduser = await findUserByEmail(username);
+      let finduser = await findUserByEmail(email);
       if (!finduser) throw new Error("Invalid Username");
       if (finduser.password !== password) throw new Error("Invalid Credentials");
       done(null, finduser);
