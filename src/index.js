1cdd0b125db8984201019e85aac342cee95b09da
Vishal Pathak <vishalpathak.er@gmail.com>
2025-04-11 11:55:33 +0530

    add mongodb database

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import passport from "passport";
 import bodyParser from "body-parser";
 import { configDotenv } from "dotenv";
 import router from "./Routes/routes.js";
+import connectDB from "./database/connection.js";
 configDotenv();
 
 // App instance
 const app = express();
 
+// Database connection
+
 // Port
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // Middlewares
 app.use(urlencoded({ extended: true }))
@@ -20,6 +23,8 @@ app.use(passport.initialize());
 app.use('/', router);
 
 
-app.listen(port, () => {
-    console.log(`Server running on ${port}`)
-})
+connectDB()
+    .then(() => { 
+            app.listen(port, () => console.log(`Server is running on port ${port}`))
+        })
+    .catch(error => console.error("Error connecting to the database:", error));
