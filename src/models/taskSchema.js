5c591e555f449ea33f26e2abeaacf13759c77c5f
Vishal Pathak <vishalpathak.er@gmail.com>
2025-04-11 12:50:19 +0530

    add the task schema by mongoose

---
+++ src/models/taskSchema.js
@@ -0,0 +1,30 @@
+import { Schema , model } from "mongoose";
+
+const taskSchema = new Schema({
+    title : {
+        type : String,
+        required : true,
+        minLength : 3,
+        maxLength : 50
+    },
+    description : {
+        type : String,
+        required : true,
+        minLength : 10,
+        maxLength : 500
+    },
+    status : {
+        type : String,
+        enum : ["Pending", "In Progress", "Completed"],
+        default : "Pending"
+    },
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    }
+}, { timestamps: true });
+
+
+const Task = model("Task", taskSchema);
+export default Task;
