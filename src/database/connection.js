7e35a76b3eb9a78b8878297013d35063d3197165
Vishal Pathak <vishalpathak.er@gmail.com>
2025-04-11 12:50:02 +0530

    update mongodb connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => { 
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB connected");
     } catch (error) {
         console.error("MongoDB connection error:", error);
@@ -14,9 +11,3 @@ const connectDB = async () => {
 }
 
 export default connectDB;
-
-// This code connects to a MongoDB database using Mongoose.
-// It exports a function called connectDB that attempts to establish a connection.
-// If the connection is successful, it logs "MongoDB connected" to the console.
-// If there is an error, it logs the error and exits the process with a failure code (1).
-// The connection URI is taken from the environment variable MONGODB_URI.
